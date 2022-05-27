87e163102b5c58da46e19964464a7cceedc579c9
Mark Coatsworth <info@markcoatsworth.com>
2022-05-26 20:37:15 -0500

    Added basic Event validation

--- src/Model/Table/EventsTable.php
+++ src/Model/Table/EventsTable.php
@@ -3,11 +3,29 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Validation\Validator;
 
 class EventsTable extends Table
 {
     public function initialize(array $config): void
     {
         $this->addBehavior('Timestamp');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->notEmptyString('title')
+            ->notEmptyString('date')
+            ->notEmptyString('city')
+            ->notEmptyString('venue');
+
+        return $validator;
     }
 }
